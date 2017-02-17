1d517a8edca7a7de6e77d4f648cac39a83d2226c
DreamAJ <dreamaj@163.com>
2017-02-17 16:25:58 +0800

    2017-2-17 噪音首页试水

--- JubaozhuSwift/JubaozhuSwift/ViewController.swift
+++ JubaozhuSwift/JubaozhuSwift/ViewController.swift
@@ -13,8 +13,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.backgroundColor = UIColor.red;
+        
+        self.createUI()
         
         
+    }
+    
+    func createUI() {
+        
+        let logoView = UIImageView(image: UIImage(named: "Logo"))
+        logoView.translatesAutoresizingMaskIntoConstraints = true
+        logoView.contentMode = UIViewContentMode.scaleAspectFill
+        self.view.addSubview(logoView)
+        
+        logoView.frame = CGRect(x: (self.view.bounds.size.width - 150 ) / 2.0 , y: 100, width: 150, height: 150)
+
+        
+        
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.isNavigationBarHidden = true;
         
     }
 
